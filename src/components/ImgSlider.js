d109ea21336533f295f807b3ca18afe8171538ed
andremiracle <andremiracle002@gmail.com>
2022-05-09 09:19:47 +0100

    ADD: grab movies from db

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -72,3 +72,10 @@ const Wrap = styled.div`
     }
 
 `
+
+
+
+
+
+
+
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,35 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
+import { selectMovies } from "../features/movie/movieSlice"
+import { useSelector } from "react-redux"
 
 function Movies() {
+    const movies = useSelector(selectMovies);
+
+
+
     return (
         <Container>
             <h4>Recommended for You</h4>
             <Content>
-                <Wrap>
-                    <img src='https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg' />
-                </Wrap>
-                <Wrap>
-                    <img src='https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg' />
-                </Wrap>
-                <Wrap>
-                    <img src='https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg' />
-                </Wrap>
-                <Wrap>
-                    <img src='https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg' />
-                </Wrap>
-                <Wrap>
-                    <img src='https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg' />
-                </Wrap>
-                <Wrap>
-                    <img src='https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg' />
-                </Wrap>
-                <Wrap>
-                    <img src='https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg' />
-                </Wrap>
-                <Wrap>
-                    <img src='https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg' />
-                </Wrap>
+                {movies &&
+                    movies.map((movie) => (
+                        <Wrap key={movie.id}>
+                            <img src={movie.cardImg} />
+                        </Wrap>
+                    ))
+                }
+
             </Content>
         </Container>
     )
